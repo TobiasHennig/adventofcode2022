7009cbb238dd1911cb80867849b83cb537edbff1
Tobias Hennig <hennig.tobias@guj.de>
2022-12-01 12:49:14 +0100

    01 - part two

--- 01/index.mjs
+++ 01/index.mjs
@@ -1,8 +1,15 @@
 import { readFile } from "node:fs/promises";
 
 const sum = (data) => data.reduce((prev, curr) => prev += parseInt(curr, 10), 0);
-const input = await readFile("input.txt", "utf-8");
-const elves = input.split("\n\n").map((value) => value.split("\n").filter((value) => !!value)).map(sum);
-const result = Math.max(...elves);
 
-console.log(`How many total Calories is that Elf carrying? ${result}`);
+const input = await readFile("input.txt", "utf-8");
+const elves = input
+  .split("\n\n")
+  .map((value) => value.split("\n")
+  .filter((value) => !!value))
+  .map(sum)
+  .sort((a, b) => b - a);
+const topThree = elves.slice(0, 3);
+
+console.log(`Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? ${topThree[0]}`);
+console.log(`Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total? ${sum(topThree)}`);
